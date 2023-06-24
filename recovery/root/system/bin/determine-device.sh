1819258dfb745f31b91f230a57dd7fa491cb6237
me-cafebabe <me.cafebabe@gmail.com>
2023-06-24 16:09:11 +0800

    moto8937: Update device determination logic

--- recovery/root/system/bin/determine-device.sh
+++ recovery/root/system/bin/determine-device.sh
@@ -2,16 +2,7 @@
 
 source /system/bin/mithorium-utils.sh
 
-workaround_gpio_flashlight() {
-	echo 1 > /sys/class/leds/flashlight/max_brightness;
-	echo 0 > /sys/class/leds/flashlight/brightness;
-}
-
-set_device_codename "$(cat /sys/xiaomi-msm8937-mach/codename)"
-set_device_model "$(cat /sys/xiaomi-msm8937-mach/product_name)"
-
-if [ "$(cat /sys/class/leds/flashlight/device/of_node/compatible)" == "qcom,leds-gpio-flash" ]; then
-	workaround_gpio_flashlight
-fi
+set_device_codename "$(getprop ro.boot.device)"
+set_device_model "$(getprop ro.boot.hardware.sku)"
 
 exit 0
