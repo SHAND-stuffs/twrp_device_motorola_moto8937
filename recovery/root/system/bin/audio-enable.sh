36df2c8a7b76d59b5eab19d6501c963bd664bea0
me-cafebabe <me.cafebabe@gmail.com>
2023-06-27 03:01:06 +0800

    moto8937: Add audio support for more devices

--- recovery/root/system/bin/audio-enable.sh
+++ recovery/root/system/bin/audio-enable.sh
@@ -4,14 +4,19 @@ ENABLE=$1
 
 #tinymix '     ' $([ "$ENABLE" == 1 ] && echo -n "1" || echo -n "0")
 
-# Enable BE DAI
-tinymix 'PRI_MI2S_RX Audio Mixer MultiMedia1' $([ "$ENABLE" == 1 ] && echo -n "1" || echo -n "0")
-
 # Enable device specific mixers
 case "$(getprop ro.product.device)" in
-	"hannah")
+	"ahannah"|"rhannah"|"aljeter")
+		tinymix 'PRI_MI2S_RX Audio Mixer MultiMedia1' $([ "$ENABLE" == 1 ] && echo -n "1" || echo -n "0")
 		tinymix "RX3 MIX1 INP1" $([ "$ENABLE" == 1 ] && echo -n "RX1" || echo -n "ZERO")
 		tinymix "SPK" $([ "$ENABLE" == 1 ] && echo -n "Switch" || echo -n "ZERO")
+		;;
+	"jeter")
+		tinymix 'QUIN_MI2S_RX Audio Mixer MultiMedia1' $([ "$ENABLE" == 1 ] && echo -n "1" || echo -n "0")
+		tinymix 'TAS2560_ALGO_FF_MODULE' $([ "$ENABLE" == 1 ] && echo -n "ENABLE" || echo -n "DISABLE")
+		;;
+	"montana")
+		tinymix 'QUIN_MI2S_RX Audio Mixer MultiMedia1' $([ "$ENABLE" == 1 ] && echo -n "1" || echo -n "0")
 		;;
 	*)
 		log "Could not enable audio, Device is unsupported!"
